0df2781a5faf86af7f54636899d4ae01db8dd95d
Assaad doumit <assaad-doumit@hotmail.com>
2021-03-17 16:47:58 +0200

    first screen done

--- PavoLive/ViewController.swift
+++ PavoLive/ViewController.swift
@@ -6,18 +6,32 @@
 //
 
 import UIKit
-import MaterialComponents.MaterialBottomAppBar
+//import MaterialComponents.MaterialBottomAppBar
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Button_SignUp: UIButton!
+    @IBOutlet weak var Button_SignIn: UIButton!
+    @IBOutlet weak var Button_Facebook: UIButton!
+    @IBOutlet weak var Button_Instagram: UIButton!
+    @IBOutlet weak var Button_Google: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = true
-        
-        
-    }
 
+        setUI()
+    }
+    
+    func setUI(){
+        Button_Google.layer.cornerRadius = 20
+        Button_Instagram.layer.cornerRadius = 20
+        Button_SignUp.layer.cornerRadius = 30
+        Button_Facebook.layer.cornerRadius = 20
+        Button_SignIn.layer.cornerRadius = 30
+    }
+    
 
 }
 
